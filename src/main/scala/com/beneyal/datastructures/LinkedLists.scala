1f745bcb4e6bbd4f3ca376f73fda7df6a7780803
Ben Eyal <ben.eyal.89@gmail.com>
2022-08-07 12:34:13 +0300

    Add doubly-linked list

--- src/main/scala/com/beneyal/datastructures/LinkedLists.scala
+++ src/main/scala/com/beneyal/datastructures/LinkedLists.scala
@@ -62,6 +62,97 @@ object LinkedLists {
       else Node(xs.head, apply(xs.tail: _*))
   }
 
+  sealed trait DoublyLinkedList {
+    def append(x: Int): DoublyLinkedList
+    def prepend(x: Int): DoublyLinkedList
+    def insert(index: Int, x: Int): DoublyLinkedList
+    def remove(index: Int): DoublyLinkedList
+    def reverse: DoublyLinkedList
+    def isEmpty: Boolean
+
+    def updateNext(newNext: => DoublyLinkedList): DoublyLinkedList
+    def updatePrev(newPrev: => DoublyLinkedList): DoublyLinkedList
+  }
+
+  object DoublyLinkedList {
+    case object Empty extends DoublyLinkedList {
+      override def append(x: Int): DoublyLinkedList = new Node(x, Empty, Empty)
+
+      override def prepend(x: Int): DoublyLinkedList = new Node(x, Empty, Empty)
+
+      override def insert(index: Int, x: Int): DoublyLinkedList =
+        throw new IllegalArgumentException(s"Index $index doesn't exist")
+
+      override def remove(index: Int): DoublyLinkedList =
+        throw new NoSuchElementException("Cannot remove from an empty list")
+
+      override def reverse: DoublyLinkedList = Empty
+
+      override def isEmpty: Boolean = true
+
+      override def toString: String = "[]"
+
+      override def updateNext(newNext: => DoublyLinkedList): DoublyLinkedList = this
+
+      override def updatePrev(newPrev: => DoublyLinkedList): DoublyLinkedList = this
+    }
+
+    final class Node(val value: Int, p: => DoublyLinkedList, n: => DoublyLinkedList) extends DoublyLinkedList {
+      lazy val prev: DoublyLinkedList = p
+      lazy val next: DoublyLinkedList = n
+
+      override def append(x: Int): DoublyLinkedList = {
+        lazy val result: DoublyLinkedList = new Node(value, prev.updateNext(result), next.append(x).updatePrev(result))
+        result
+      }
+
+      override def prepend(x: Int): DoublyLinkedList = {
+        lazy val result: DoublyLinkedList = new Node(x, Empty, this.updatePrev(result))
+        result
+      }
+
+      override def insert(index: Int, x: Int): DoublyLinkedList =
+        if (index == 0) prepend(x)
+        else new Node(value, prev, next.insert(index - 1, x))
+
+      override def remove(index: Int): DoublyLinkedList =
+        if (index == 0) next
+        else new Node(value, prev, next.remove(index - 1))
+
+      override def reverse: DoublyLinkedList =
+        if (next.isEmpty) this
+        else next.reverse.append(value)
+
+      override def isEmpty: Boolean = false
+
+      override def toString: String = {
+        @tailrec
+        def loop(list: DoublyLinkedList, parts: Vector[String]): String =
+          list match {
+            case Empty      => parts.mkString("[", ", ", "]")
+            case node: Node => loop(node.next, parts :+ node.value.toString)
+          }
+
+        loop(this, Vector.empty)
+      }
+
+      override def updateNext(newNext: => DoublyLinkedList): DoublyLinkedList = {
+        lazy val result: DoublyLinkedList = new Node(value, prev.updateNext(result), newNext)
+        result
+      }
+
+      override def updatePrev(newPrev: => DoublyLinkedList): DoublyLinkedList = {
+        lazy val result: DoublyLinkedList = new Node(value, newPrev, next.updatePrev(result))
+        result
+      }
+    }
+
+    def apply(xs: Int*): DoublyLinkedList =
+      xs.foldLeft(empty)(_.append(_))
+
+    def empty: DoublyLinkedList = Empty
+  }
+
   def main(args: Array[String]): Unit = {
     val list = LinkedList(1, 2, 3, 4)
     println(s"list = $list")
@@ -70,5 +161,15 @@ object LinkedLists {
     println(s"list.insert(3, 42) = ${list.insert(3, 42)}")
     println(s"list.remove(3) = ${list.remove(3)}")
     println(s"list.reverse = ${list.reverse}")
+
+    println()
+
+    val dllist = DoublyLinkedList(1, 2, 3, 4)
+    println(s"dllist = $dllist")
+    println(s"dllist.append(5) = ${dllist.append(5)}")
+    println(s"dllist.prepend(0) = ${dllist.prepend(0)}")
+    println(s"dllist.insert(3, 42) = ${dllist.insert(3, 42)}")
+    println(s"dllist.remove(3) = ${dllist.remove(3)}")
+    println(s"dllist.reverse = ${dllist.reverse}")
   }
 }
